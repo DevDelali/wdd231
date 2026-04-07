f002fdb8dd17ace05c4970929e00adf952bfed49
DevDelali <delalpaul5@gmail.com>
2026-04-07 18:06:00 +0000

    added details to the webpages . modified them with css, met some of the requirements

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -4,13 +4,13 @@ console.log(places);
 // create a card for the items in the places array and add them to the page
 const placesContainer = document.getElementById('places-container');
 places.forEach(place => {
-    const card = document.createElement('div');
+    const card = document.createElement('section');
     card.classList.add('place-card');
     card.innerHTML = `
         <h2>${place.name}</h2>
-        <img src="${place.photo_url}" alt="${place.name}">
         <p>${place.description}</p>
         <p><strong>Address:</strong> ${place.address}</p>
+        <img src="${place.photo_url}" alt="${place.name}">
     `;
     placesContainer.appendChild(card);
 })
